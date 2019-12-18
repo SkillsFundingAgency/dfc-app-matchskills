64302fc1473f7e2dd1fed43fe69fe8d261d90a5f
Liam Wright <mail@liam-wright.co.uk>
2019-12-18 13:49:52 +0000

    Added components to use with Composite UI

---
+++ DFC.App.MatchSkills.WebUI/ViewModels/HeadViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFC.App.MatchSkills.WebUI.ViewModels
+{
+    public class HeadViewModel
+    {
+        public string Title { get; set; }
+
+        public string CanonicalUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string Keywords { get; set; }
+
+        public string CssLink { get; set; }
+    }
+}
