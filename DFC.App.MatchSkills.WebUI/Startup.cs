b3a9eeddc6df60e008ace257b3de5bfcb010b383
Liam Wright <mail@liam-wright.co.uk>
2019-12-20 11:25:06 +0000

    Startup cleanup, and implemented button tags

--- DFC.App.MatchSkills.WebUI/Startup.cs
+++ DFC.App.MatchSkills.WebUI/Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DFC.App.MatchSkills.WebUI
 {
@@ -26,19 +18,12 @@ namespace DFC.App.MatchSkills.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddControllers();
-            //services.AddMvc();
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
-            var options = new RewriteOptions()
-                .AddRewrite(@"^assets/(.*)", "_content/DFC.Personalisation.CommonUI/assets/$1", skipRemainingRules: true);
-            app.UseRewriter(options);
-            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -46,14 +31,10 @@ namespace DFC.App.MatchSkills.WebUI
 
             app.UseHttpsRedirection();
             app.UseRouting();
-            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-               // endpoints.MapControllers();
+                endpoints.MapControllers();
             });
         }
     }
