49eef7052ba0fef1e2c9911156337f7bccc30d1d
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-01-29 14:22:51 +0000

    Update AutoComplete and Added key delay

--- DFC.App.MatchSkills.Services.ServiceTaxonomy/AutoMapperConfiguration.cs
+++ DFC.App.MatchSkills.Services.ServiceTaxonomy/AutoMapperConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using AutoMapper;
 using DFC.App.MatchSkills.Services.ServiceTaxonomy.Models;
+using DFC.Personalisation.Common.Extensions;
 using DFC.Personalisation.Domain.Models;
 
 namespace DFC.App.MatchSkills.Services.ServiceTaxonomy
@@ -26,19 +27,19 @@ namespace DFC.App.MatchSkills.Services.ServiceTaxonomy
         {
             CreateMap<StSkill, Skill>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Uri))
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Skill))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Skill.FirstCharToUpper()))
                 .ForMember(dest => dest.AlternativeNames, opt => opt.MapFrom(src => src.AlternativeLabels))                     
                 .ConstructUsing(dest => new Skill(dest.Uri, dest.Skill, (SkillType)Enum.Parse(typeof(SkillType),dest.SkillType,true)))
                 ;
             CreateMap<StOccupation, Occupation>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Uri))
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Occupation))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Occupation.FirstCharToUpper()))
                 .ForMember(dest => dest.AlternativeNames, opt => opt.MapFrom(src => src.AlternativeLabels))                     
                 .ConstructUsing(dest => new Occupation(dest.Uri, dest.Occupation,dest.LastModified));
             
             CreateMap<StOccupationSearchResult.StsOccupation, Occupation>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Uri))
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Occupation))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Occupation.FirstCharToUpper()))
                 .ForMember(dest => dest.AlternativeNames, opt => opt.MapFrom(src => src.AlternativeLabels))                     
                 .ConstructUsing(dest => new Occupation(dest.Uri, dest.Occupation,dest.LastModified));
         }
