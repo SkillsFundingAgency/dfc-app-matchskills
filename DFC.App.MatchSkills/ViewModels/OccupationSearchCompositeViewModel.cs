7a04da2620a26173171808b8d7468303c1f1a4f0
Matt Daniels <matt@techtoniq.com>
2020-02-03 12:06:16 +0000

    MS-155 id for search button

---
+++ DFC.App.MatchSkills/ViewModels/OccupationSearchCompositeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DFC.App.MatchSkills.ViewModels
+{
+    public class OccupationSearchViewModel : CompositeViewModel
+    {
+        public OccupationSearchViewModel()
+            : base(PageId.OccupationSearch, "Enter your current or previous job title")
+        {
+        }
+
+        public string SearchService { get; set; }
+    }
+}
--- DFC.App.MatchSkills/ViewModels/OccupationSearchViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DFC.App.MatchSkills.ViewModels
-{
-    public class OccupationSearchViewModel
-    {
-        public string SearchService { get; set; }
-    }
-}
