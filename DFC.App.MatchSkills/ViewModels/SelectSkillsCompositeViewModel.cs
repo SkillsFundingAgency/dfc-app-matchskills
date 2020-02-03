c2954470e76cbb4dad7bdc9cc0bc6880f5abae90
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-02-03 09:35:56 +0000

    WIP

--- DFC.App.MatchSkills/ViewModels/SelectSkillsCompositeViewModel.cs
+++ DFC.App.MatchSkills/ViewModels/SelectSkillsCompositeViewModel.cs
@@ -9,7 +9,7 @@ namespace DFC.App.MatchSkills.ViewModels
     public class SelectSkillsCompositeViewModel : CompositeViewModel
     {
         public string Occupation;
-        public IEnumerable<Skill> Skills;
+        public ICollection<Skill> Skills;
         public SelectSkillsCompositeViewModel()  : base(PageId.SelectSkills, "Select your skills")
         {
             
