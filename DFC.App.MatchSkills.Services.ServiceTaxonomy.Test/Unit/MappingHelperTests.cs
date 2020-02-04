6bbc36ebea86764c33865fef739a8618a0b163eb
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-02-04 11:43:20 +0000

    Added tests

--- DFC.App.MatchSkills.Services.ServiceTaxonomy.Test/Unit/MappingHelperTests.cs
+++ DFC.App.MatchSkills.Services.ServiceTaxonomy.Test/Unit/MappingHelperTests.cs
@@ -11,6 +11,8 @@ namespace DFC.App.MatchSkills.Services.ServiceTaxonomy.Test.Unit
     class MappingHelperTests
     {
         [TestCase( "http://data.europa.eu/esco/skill/7ff9cce1-6518-48db-8f59-2beb81508fb2", ExpectedResult="7ff9cce1-6518-48db-8f59-2beb81508fb2")]
+        [TestCase( "", ExpectedResult="")]
+        [TestCase( null, ExpectedResult="")]
         public string When_GetIdFromUrl_ReturnsGuidId(string url)
         {
             return  MappingHelper.GetIdFromUrl(url);
