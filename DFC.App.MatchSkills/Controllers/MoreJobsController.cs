735c048cd617f2a79f9b1166fc36186c65161901
Liam Wright <mail@liam-wright.co.uk>
2020-02-12 14:11:17 +0000

    Added endpoint hit test

--- DFC.App.MatchSkills/Controllers/MoreJobsController.cs
+++ DFC.App.MatchSkills/Controllers/MoreJobsController.cs
@@ -1,15 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Dfc.ProviderPortal.Packages;
 using DFC.App.MatchSkills.Application.ServiceTaxonomy;
 using DFC.App.MatchSkills.Application.Session.Interfaces;
 using DFC.App.MatchSkills.Models;
 using DFC.App.MatchSkills.Services.ServiceTaxonomy;
 using DFC.App.MatchSkills.Services.ServiceTaxonomy.Models;
 using DFC.App.MatchSkills.ViewModels;
-using DFC.Personalisation.Domain.Models;
-using Dfc.ProviderPortal.Packages;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -18,20 +13,16 @@ namespace DFC.App.MatchSkills.Controllers
 {
     public class MoreJobsController : CompositeSessionController<MoreJobsViewModel>
     {
-        private readonly IServiceTaxonomySearcher _serviceTaxonomy;
         private readonly ServiceTaxonomySettings _settings;
 
         public MoreJobsController(IDataProtectionProvider dataProtectionProvider,
-            IServiceTaxonomySearcher serviceTaxonomy,
             IOptions<ServiceTaxonomySettings> settings,
             IOptions<CompositeSettings> compositeSettings,
             ISessionService sessionService)
             : base(dataProtectionProvider, compositeSettings,
                 sessionService)
         {
-            Throw.IfNull(serviceTaxonomy, nameof(serviceTaxonomy));
             Throw.IfNull(settings, nameof(settings));
-            _serviceTaxonomy = serviceTaxonomy ?? new ServiceTaxonomyRepository();
             _settings = settings.Value;
         }
 
