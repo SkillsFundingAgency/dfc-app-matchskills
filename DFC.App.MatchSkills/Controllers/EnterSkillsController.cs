1444c672196b2618542c60de79e3ce1b6d531217
Liam Wright <mail@liam-wright.co.uk>
2020-02-17 11:05:24 +0000

    Getting latest fix

--- DFC.App.MatchSkills/Controllers/EnterSkillsController.cs
+++ DFC.App.MatchSkills/Controllers/EnterSkillsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DFC.App.MatchSkills.Application.Session.Interfaces;
+using DFC.App.MatchSkills.Interfaces;
 using DFC.App.MatchSkills.Models;
 using DFC.App.MatchSkills.ViewModels;
 using Microsoft.AspNetCore.DataProtection;
@@ -13,18 +14,19 @@ namespace DFC.App.MatchSkills.Controllers
 {
     public class EnterSkillsController : CompositeSessionController<EnterSkillsCompositeViewModel>
     {
-        public EnterSkillsController(IDataProtectionProvider dataProtectionProvider, 
-            IOptions<CompositeSettings> compositeSettings, ISessionService sessionService) 
-            : base(dataProtectionProvider, compositeSettings, sessionService)
+        public EnterSkillsController(IOptions<CompositeSettings> compositeSettings, ISessionService sessionService, ICookieService cookieService)
+            : base(compositeSettings, sessionService, cookieService)
         {
         }
         public override async Task<IActionResult> Body()
         {
             await TrackPageInUserSession();
             var userSession = await GetUserSession();
-            ViewModel.Skills.LoadFromSession(userSession);
+            ViewModel.Skills.LoadFrom(userSession);
             return await base.Body();
 
         }
+
+   
     }
 }
