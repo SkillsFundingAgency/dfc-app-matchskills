dd002a2161cc74ddbdaa3bdfac7a5ec0de02ac25
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-02-20 15:40:11 +0000

    Removed basecontroller

--- DFC.App.MatchSkills/Controllers/OccupationSearchResultsController.cs
+++ DFC.App.MatchSkills/Controllers/OccupationSearchResultsController.cs
@@ -1,44 +1,38 @@
-﻿using Microsoft.AspNetCore.DataProtection;
+﻿using DFC.App.MatchSkills.Application.ServiceTaxonomy;
+using DFC.App.MatchSkills.Application.Session.Interfaces;
+using DFC.App.MatchSkills.Interfaces;
+using DFC.App.MatchSkills.Models;
+using DFC.App.MatchSkills.Services.ServiceTaxonomy;
+using DFC.App.MatchSkills.Services.ServiceTaxonomy.Models;
+using DFC.App.MatchSkills.ViewModels;
+using Dfc.ProviderPortal.Packages;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DFC.App.MatchSkills.Controllers
 {
 
-    public class OccupationSearchResultsController : BaseController
+    public class OccupationSearchResultsController : CompositeSessionController<OccupationSearchResultsCompositeViewModel>
     {
         private const string PathName = "OccupationSearchResults";
-
-        public OccupationSearchResultsController(IDataProtectionProvider dataProtectionProvider) : base()
+        private readonly IServiceTaxonomySearcher _serviceTaxonomy;
+        private readonly ServiceTaxonomySettings _settings;
+        private readonly ISessionService _sessionService;
+        public OccupationSearchResultsController(IServiceTaxonomySearcher serviceTaxonomy, 
+            IOptions<ServiceTaxonomySettings> settings,
+            IOptions<CompositeSettings> compositeSettings,
+            ISessionService sessionService, ICookieService cookieService) 
+            : base(compositeSettings,
+                sessionService, cookieService)
         {
+            Throw.IfNull(serviceTaxonomy, nameof(serviceTaxonomy));
+            Throw.IfNull(settings, nameof(settings));
+            _serviceTaxonomy = serviceTaxonomy ?? new ServiceTaxonomyRepository();
+            _settings = settings.Value;
+            _sessionService = sessionService;
         }
 
-        [HttpGet]
-        [Route("/head/OccupationSearchResults")]
-        public override IActionResult Head()
-        {
-            return View(ReturnPath("Head", "OccupationSearchResults"));
-        }
-
-        [HttpGet]
-        [Route("/breadcrumb/"+ PathName)]
-        public override IActionResult Breadcrumb()
-        {
-            return View(ReturnPath("Breadcrumb", "OccupationSearchResults"));
-        }
-
-        [HttpGet]
-        [Route("/bodytop/"+ PathName)]
-        public override IActionResult BodyTop()
-        {
-            return View(ReturnPath("bodytop"));
-        }
-
-        [SessionRequired]
-        [HttpGet]
-        [Route("/body/"+ PathName)]
-        public override IActionResult Body()
-        {
-            return View(ReturnPath("body", "OccupationSearchResults"));
-        }
+       
     }
 }
