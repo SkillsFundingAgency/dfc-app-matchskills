81cd6c7030e2e408798b49c593ca9fa8d71e1c47
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-02-20 15:49:56 +0000

    tidy up

--- DFC.App.MatchSkills/Controllers/OccupationSearchResultsController.cs
+++ DFC.App.MatchSkills/Controllers/OccupationSearchResultsController.cs
@@ -2,12 +2,8 @@
 using DFC.App.MatchSkills.Application.Session.Interfaces;
 using DFC.App.MatchSkills.Interfaces;
 using DFC.App.MatchSkills.Models;
-using DFC.App.MatchSkills.Services.ServiceTaxonomy;
 using DFC.App.MatchSkills.Services.ServiceTaxonomy.Models;
 using DFC.App.MatchSkills.ViewModels;
-using Dfc.ProviderPortal.Packages;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
 namespace DFC.App.MatchSkills.Controllers
@@ -15,10 +11,7 @@ namespace DFC.App.MatchSkills.Controllers
 
     public class OccupationSearchResultsController : CompositeSessionController<OccupationSearchResultsCompositeViewModel>
     {
-        private const string PathName = "OccupationSearchResults";
-        private readonly IServiceTaxonomySearcher _serviceTaxonomy;
-        private readonly ServiceTaxonomySettings _settings;
-        private readonly ISessionService _sessionService;
+        
         public OccupationSearchResultsController(IServiceTaxonomySearcher serviceTaxonomy, 
             IOptions<ServiceTaxonomySettings> settings,
             IOptions<CompositeSettings> compositeSettings,
@@ -26,11 +19,7 @@ namespace DFC.App.MatchSkills.Controllers
             : base(compositeSettings,
                 sessionService, cookieService)
         {
-            Throw.IfNull(serviceTaxonomy, nameof(serviceTaxonomy));
-            Throw.IfNull(settings, nameof(settings));
-            _serviceTaxonomy = serviceTaxonomy ?? new ServiceTaxonomyRepository();
-            _settings = settings.Value;
-            _sessionService = sessionService;
+            
         }
 
        
