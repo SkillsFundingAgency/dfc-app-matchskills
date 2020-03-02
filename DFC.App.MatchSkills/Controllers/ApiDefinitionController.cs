56cda06c80d7b9da664304a9edb967f71b1aa906
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-03-02 09:47:21 +0000

    Update API-Definition

--- DFC.App.MatchSkills/Controllers/ApiDefinitionController.cs
+++ DFC.App.MatchSkills/Controllers/ApiDefinitionController.cs
@@ -1,14 +1,20 @@
 ﻿using DFC.App.MatchSkills.Interfaces;
+using DFC.App.MatchSkills.Models;
+using DFC.App.MatchSkills.Services.ServiceTaxonomy.Models;
+using Dfc.ProviderPortal.Packages;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DFC.App.MatchSkills.Controllers
 {
     public class ApiDefinitionController : Controller
     {
         private readonly IFileService _fileService;
-        public ApiDefinitionController(IFileService fileService)
+       
+        public ApiDefinitionController(IFileService fileService,  IOptions<ServiceTaxonomySettings> settings)
         {
             _fileService = fileService;
+       
         }
 
         [HttpGet]
@@ -16,7 +22,9 @@ namespace DFC.App.MatchSkills.Controllers
        
         public IActionResult Index()
         {
+            var hostName = Request.Headers["HOSTNAME"].ToString();
             var apiDefinition = _fileService.ReadAllText(@"Docs\OccupationSearchAuto.json");
+            apiDefinition = apiDefinition.Replace("{serverurl}", $"{hostName}.azurewebsites.net");
             if (string.IsNullOrEmpty(apiDefinition))
                 return NoContent();
 
