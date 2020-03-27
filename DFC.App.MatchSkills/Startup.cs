ef6d34fbe7d605fd5f7334bdc83d0fea1deb1449
Liam Wright <mail@liam-wright.co.uk>
2020-03-27 15:28:24 +0000

    Removed last code smell

--- DFC.App.MatchSkills/Startup.cs
+++ DFC.App.MatchSkills/Startup.cs
@@ -26,7 +26,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NSubstitute.Extensions;
 
 
 namespace DFC.App.MatchSkills
