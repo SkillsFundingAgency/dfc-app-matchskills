e6d9e0ceab0a5c64fa431186ce9204dd31f72061
Matt Daniels <matt@techtoniq.com>
2020-04-07 16:26:43 +0100

    Additional test for ViewModel

--- DFC.App.MatchSkills.Test/Unit/Controllers/ErrorControllerTests.cs
+++ DFC.App.MatchSkills.Test/Unit/Controllers/ErrorControllerTests.cs
@@ -1,6 +1,9 @@
 ﻿using DFC.App.MatchSkills.Application.Session.Interfaces;
+using DFC.App.MatchSkills.Application.Session.Models;
 using DFC.App.MatchSkills.Controllers;
 using DFC.App.MatchSkills.Models;
+using DFC.App.MatchSkills.Test.Helpers;
+using DFC.App.MatchSkills.ViewModels;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -37,6 +40,26 @@ namespace DFC.App.MatchSkills.Test.Unit.Controllers
             result.Should().NotBeNull();
             result.Should().BeOfType<ViewResult>();
             result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public async Task When_SessionEmpty_Then_ViewModelHasNoRTACode()
+        {
+            var controller = new ErrorController(_compositeSettings, _sessionService);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            _sessionService.GetUserSession().ReturnsForAnyArgs(MockHelpers.GetUserSession(false));
+
+            await controller.Body();
+
+            var result = await controller.Body() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+
+            ((ErrorCompositeViewModel)result.ViewData.Model).RTACode.Should().Be(string.Empty);
         }
     }
 }
