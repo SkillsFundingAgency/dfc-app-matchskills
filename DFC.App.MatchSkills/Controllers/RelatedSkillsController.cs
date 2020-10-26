3cb996d3889078151d7a226535bd86fef4811cac
Tom Tollafield <thomas.tollafield@gmail.com>
2020-10-26 07:37:12 +0000

    - Don't filter skills

--- DFC.App.MatchSkills/Controllers/RelatedSkillsController.cs
+++ DFC.App.MatchSkills/Controllers/RelatedSkillsController.cs
@@ -114,8 +114,8 @@ namespace DFC.App.MatchSkills.Controllers
             {
                 var skills = await _serviceTaxonomy.GetSkillsByLabel<Skill[]>($"{_apiUrl}",
                     _apiKey, ViewModel.SearchTerm);
-                List<Skill> filteredSkills = skills.Where(x => x.RelationshipType == RelationshipType.Essential).ToList();
-                ViewModel.RelatedSkills.LoadFrom(filteredSkills);
+                //List<Skill> filteredSkills = skills.Where(x => x.RelationshipType == RelationshipType.Essential).ToList();
+                ViewModel.RelatedSkills.LoadFrom(skills);
             }
         }
 
